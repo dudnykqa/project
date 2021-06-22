73bc1ea87d3c110968a05eaa40afa73ec718aa5e
Denys <example@gmail.com>
2021-06-22 21:49:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//alert('hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("What is your age?", "18");
+//console.log(answer + 5);
+
+//const answers = [];
+//answers [0] = prompt('What is your name?', '');
+//answers [1] = prompt('What is your surname?', '');
+//answers [2] = prompt('How old are you?', '');
+
+//document.write(answers);
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Ivan";
+//alert (`Hello, ${user}`);
+    
+console.log('arr' + " - object" );
+
+ const isChecked = false,
+       isClose = false;
+console.log(isChecked || !isClose);
+
+console.log (2 + 2 * 2 == 8);
